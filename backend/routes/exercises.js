1f9db2be5213e00901986596eff83cd39bfd48d3
Mahimul Hoque <mahimulh13@gmail.com>
2021-03-16 21:55:59 -0400

    simple CRUD routes in exercises.js

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,3 +1,5 @@
+//simple CRUD routes done in this page
+
 const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
@@ -24,5 +26,33 @@ router.route('/add').post((req,res) => {
         .then(() => res.json('Exercise added!'))
         .then(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/:id').get((req, res) => { //read
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => { //delete
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req,res) => { //update
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 
 module.exports = router;
